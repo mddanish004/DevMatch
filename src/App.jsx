792702576ba1a1b03953ab7f845f3f169c4ecbdd
Md Danish Ansari <m.danishansari004@gmail.com>
2025-05-27 04:08:32 +0530

    Installed tailwind and daisyUi

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1>hello world</h1>
+      <h1 className="text-4xl text-yellow-400 text-center justify-center items-center">hello world</h1>
     </>
   )
 }
